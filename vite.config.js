686ee0d7281deba61fe00fb35ce1b4fb65162354
youssifhegab <youssif.hegab@zyda.com>
2023-05-20 02:57:15 +0300

    Add imagemin to compress images

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,39 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import viteImagemin from "vite-plugin-imagemin";
 
 export default defineConfig({
-  plugins: [react()],
   server: {
     port: 3000,
   },
+  plugins: [
+    react(),
+    viteImagemin({
+      gifsicle: {
+        optimizationLevel: 7,
+        interlaced: false,
+      },
+      optipng: {
+        optimizationLevel: 7,
+      },
+      mozjpeg: {
+        quality: 20,
+      },
+      pngquant: {
+        quality: [0.8, 0.9],
+        speed: 4,
+      },
+      svgo: {
+        plugins: [
+          {
+            name: "removeViewBox",
+          },
+          {
+            name: "removeEmptyAttrs",
+            active: false,
+          },
+        ],
+      },
+    }),
+  ],
 });
